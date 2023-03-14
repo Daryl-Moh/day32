10cd6562d1ba13d73c81c0eb2d93918640c3495e
Daryl_Moh <Daryl.Moh@hotmail.com>
2023-03-14 22:13:48 +0800

    first commmit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { RegistrationComponent } from './components/registration.component';
+import { TodoComponent } from './components/todo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegistrationComponent,
+    TodoComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule, ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/models.ts
@@ -0,0 +1,18 @@
+export interface RSVP {
+    name: string
+    email: string
+    age: number
+    attendance: boolean
+
+}
+
+export interface Task {
+    description: string
+    dueDate: string
+}
+
+export interface Activities {
+    taskName: string
+    name: string
+    tasks: Task[]
+}
